3c925dddf797d7aca421fc31e93a7708bba1beb7
Ruslan Malyavsky <ruslan.malyavsky@gmail.com>
2021-12-30 11:10:32 +0200

    Changed menu view

--- app/containers/MainMenu.js
+++ app/containers/MainMenu.js
@@ -1,9 +1,14 @@
 import React from "react";
 import itemsData from "../data/itemsData.js"
 import Item from "../components/Item";
+import '../styles/MainMenu.scss';
 
 export default function MainMenu(){
     const items = itemsData.map(item => <Item className="item" item={item} key={item.id}/>)
     
-    return <div>{items}</div>
+    return (<nav className="nav-menu">
+                <ul>
+                    {items}
+                </ul>
+            </nav>)
 }
